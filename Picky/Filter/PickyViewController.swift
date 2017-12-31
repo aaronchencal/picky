2994a5e78c34082146ecab0fc33f71f583dd3509
aaronchendev <mainaccount@Aarons-Air.attlocal.net>
2017-12-30 22:29:09 -0800

    Added first filter, fixed segueing

---
+++ Picky/Filter/PickyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PickyViewController.swift
+//  Picky
+//
+//  Created by Aaron Chen on 12/30/17.
+//  Copyright © 2017 ACDev. All rights reserved.
+//
+
+import UIKit
+
+class PickyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
